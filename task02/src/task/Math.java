9588fff227cf2e370b488606d9aaab8b28ac8483
K.Sivabalan <ksivabalan.91@gmail.com>
2023-01-27 11:03:23 +0800

    Task 02 complete

--- task02/src/task/Math.java
+++ task02/src/task/Math.java
@@ -2,11 +2,27 @@ package task;
 
 public class Math {
 
+    private static float last=0.0f;
+
     public String calculate(String problem){
         String[] initial = problem.trim().split(" ",-1);
         Float result = 0f;
-        Float operand_1 = Float.parseFloat(initial[0]);
-        Float operand_2 = Float.parseFloat(initial[2]);
+        Float operand_1;
+        Float operand_2;
+        
+        if(initial[0].equals("$last")){
+            operand_1 = last;
+        } else{
+            operand_1 = Float.parseFloat(initial[0]);
+        }
+        
+        if(initial[2].equals("$last")){
+            operand_2 = last;
+        } else{
+            operand_2 = Float.parseFloat(initial[2]);            
+        }
+        
+        
         String operator = initial[1];
 
         switch(operator){
@@ -27,7 +43,8 @@ public class Math {
 
         }
 
-        String ans = result.toString();                     
+        last = result;                     
+        String ans = result.toString();
 
         return ans;
 
