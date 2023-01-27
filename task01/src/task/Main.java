a399456a8c4ffabfd1f6dd32d2f0e261306216f8
K.Sivabalan <ksivabalan.91@gmail.com>
2023-01-27 10:55:14 +0800

    task02 initial commit

--- task01/src/task/Main.java
+++ task01/src/task/Main.java
@@ -3,23 +3,59 @@ package task;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) throws IOException{
 
         List<String> data = new LinkedList<>();
+        List<String> wordsList = new LinkedList<>();
+        List<Double> wordCount = new LinkedList<>();
         String filename = "cat_in_the_hat.txt";
+        Set<String> words = new HashSet<>();
+        Map<String,Double> wordCountMap = new HashMap<>();
+
 
         if(args.length>0){filename = args[0];}
 
+        // read file and store in string list
         data = readFile(filename);
+        // remove duplicates
+        for(String i : data){words.add(i);} 
+    
 
-        for(String i : data){
+        for(String i: words){
+            int counter =0;
+            for(String j : data){
+                if(i.equals(j)){
+                    counter++;
+                }
+            }
+            double freq = ((double) counter) / ((double)data.size());
+            wordsList.add(i);
+            wordCount.add(freq);
+            wordCountMap.put(i, freq);
+        }    
+        System.out.println(wordsList);
+        System.out.println(wordCount);
+
+        List<Map.Entry<String,Double>> list = new ArrayList<>(wordCountMap.entrySet());
+
+        Collections.sort(list, new Comparator<Map.Entry<String, Double>>() {
             
-        }
-        System.out.println(data.toString());
+        })
+
+
+        // System.out.println(wordCountMap.toString());
+        // System.out.println(words.toString());
 
         
     }
@@ -47,7 +83,7 @@ public class Main {
 
             for(String i: lineArr){
                 if(!i.equals("")){
-                    tempList.add(i.trim());                
+                    tempList.add(i.toLowerCase().trim());                
                 }
             }            
         }
--- task02/src/task/Main.java
+++ task02/src/task/Main.java
@@ -1,5 +1,32 @@
 package task;
 
+import java.io.Console;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
 public class Main {
+    public static void main(String[] args) {
+        Boolean exit = true;
+        Console cons = System.console();
+        Math operation = new Math();  
+
+        System.out.println("Welcome.");
+
+        while(exit){
+            String line = cons.readLine("> ");
+
+            if(line.contentEquals("exit")){
+                exit = false;
+                System.out.println("Bye bye");
+                continue;
+            }
+            String output = operation.calculate(line);
+            System.out.println(output);
+
+        }   
+    }
     
 }
